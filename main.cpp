3d16e355b24b4820ad8ad2f1e52839a0c39f042c
Ezequiel <ezequielleonzybert@gmail.com>
2025-10-07 18:12:36 -0300

    logo

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     Parametrikut window;
+    window.setWindowIcon(QIcon(":/Parametrikut/logo/logo.png"));
     window.show();
     return app.exec();
 }
